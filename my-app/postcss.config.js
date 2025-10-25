6622ff33bb3f9b8643d74274311bf37a6d1d6514
highsparroww <nigamakprateek@gmail.com>
2025-10-25 07:56:13 -0700

    Revert "Optimize Tailwind purge and Vite CSS build for performance"
    This reverts commit bec6e2666093ae4bb5c021017dbd54f034c7e82c.

--- my-app/postcss.config.js
+++ my-app/postcss.config.js
@@ -1,16 +1,6 @@
-// postcss.config.js
 export default {
   plugins: {
-    tailwindcss: {
-      content: [
-        './index.html',
-        './src/**/*.{js,ts,jsx,tsx}', // scan all React files
-      ],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
